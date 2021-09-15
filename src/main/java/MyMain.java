a7b0b42778c38064b8ff7e97f80a2f8a306434af
parkerschwantes <23pschwantes@athenian.org>
2021-09-14 17:15:52 -0700

    I don't know what is wrong

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -17,21 +17,36 @@ public class MyMain {
             binary_decimal /= 10;
             n *= 10;
         }
-        return -1;
+        System.out.println(final_num);
+        return final_num;
     }
 
     // Given a decimal number (e.g. 152) as input, and returns a String
     // representing the binary representation of that number.
     // Ex. decimalToBinary(7) => "111"
     //     decimalToBinary(152) => "10011000"
-    public static String decimalToBinary(int decimal) {
-        // REPLACE WITH YOUR CODE
-        return "";
+    public static int decimalToBinary(int decimal) {
+        int m = 1;
+        int final_things = 0;
+        while (decimal > 0) {
+            int holder = decimal % 2;
+            final_things = (holder * m) + final_things;
+            decimal /= 2;
+            m *= 10;
+        }
+        System.out.println(final_things);
+        return final_things;
     }
     
     
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        // binaryToDecimal(binary);
+        System.out.println("Enter a number in binary: ");
+        String binary = scan.nextLine();
+        binaryToDecimal(binary);
+
+        System.out.println("Enter a number in decimal: ");
+        int decimal = scan.nextInt();
+        decimalToBinary(decimal);
     }
 }
